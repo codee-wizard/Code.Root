e12aac0e45920a0c5a2f9ca819d1fe44f37c13b5
Apoorv Choudhary <apoorvachoudhary506@gmail.com>
2025-05-06 11:30:21 +0530

    htmldone

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,42 @@
 import React from "react";
 import "../styles/Login.css"
 
+import user_icon from '../components/Assets/person.png'
+import email_icon from '../components/Assets/email.png'
+import password_icon from '../components/Assets/password.png'
+
 const loginSignup =() =>{
     return(
-        <div>
-            
+        <div className="container">
+            <div className="header">
+                <div className="text">Sign Up</div>
+                <div className="underline"></div>
+            </div>
+            <div className="inputs">
+                <div className="input">
+                    <img src={user_icon} alt=""/>
+                    <input type="text"/>
+                </div>
+            </div>
+            <div className="inputs">
+                <div className="input">
+                    <img src={email_icon} alt=""/>
+                    <input type="email"/>
+                </div>
+            </div>
+            <div className="inputs">
+                <div className="input">
+                    <img src={password_icon} alt=""/>
+                    <input type="password"/>
+                </div>
+            </div>
+            <div className="forgot-pass">Forgot Password?<span>Click Here!</span></div>
+            <div className="submit-cnt">
+                <div className="submit">Sign Up</div>
+                <div className="submit">Login</div>
+            </div>
         </div>
     )
 }
+
+export default loginSignup;
