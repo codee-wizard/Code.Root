abf81eaa2adcbe51c3dc7110897e30fa717b2fa2
Apoorv Choudhary <apoorvachoudhary506@gmail.com>
2025-05-06 08:14:07 +0530

    all-set

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-
+import React from 'react';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
+      <Home/>
+      <Footer/>
     </div>
   );
 }
